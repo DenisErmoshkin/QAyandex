f04dfef2e07e90e30a18c4cc8c55bbf381e36fe0
dener1974 <dener1974@icloud.com>
2023-07-10 19:10:47 +0200

    asdfgh

--- pages_models.py
+++ pages_models.py
@@ -53,36 +53,87 @@ class RegPage:
 
         return button_reg, driver
 
-    def negative_tests_form(self, test_date):
+    def negative_test_form(self, test_date):
         locator = self.__get_locators(test_date)
         # button_reg.click()
         locator[1].execute_script("arguments[0].click();", locator[0])
         # passw.send_keys(Keys.ENTER)
+        time.sleep(1)
         try:
             alert = wait(locator[1], 10).until(EC.presence_of_element_located(RegPage.ALERT))
         except:
             pass
         # url = driver.current_url
         assert alert != False, 'test is failed, NO ALERT'
-        locator[1].quit()
+        # locator[1].quit()
 
-    def positive_tests_form(self, test_date):
+    def positive_test_form(self, test_date):
         locator = self.__get_locators(test_date)
         # button_reg.click()
         locator[1].execute_script("arguments[0].click();", locator[0])
         # passw.send_keys(Keys.ENTER)
+        time.sleep(1)
         try:
             alert = wait(locator[1], 3).until(EC.presence_of_element_located(RegPage.ALERT))
         except:
             alert = False
             pass
         # url = driver.current_url
-        assert alert == False, 'test is failed, NO ALERT'
-        locator[1].quit()
+
+        assert alert == False, 'test is failed, RISE ALERT'
+        # print(alert)
+        # locator[1].quit()
 
 
 q = RegPage()
 # [firstname, lastname, password, password-confirm, phone namber]
-q.negative_tests_form(['qwe', ' ', 'ermqwe', 'Qwertyuiop[1974', 'Qwertyuiop[1974', '+420773288247'])
-q.positive_tests_form(['qwe', 'asd', 'ermqwe', 'Qwertyuiop[1974', 'Qwertyuiop[1974', '+420773288247'])
+try:
+    q.negative_test_form(['', 'qwe', 'ermqwe', 'Qwertyuiop[1974', 'Qwertyuiop[1974', '+79223332233'])
+    print('OK - Пустое поле для ввода имени, NEGATIVE TEST')
+except:
+    print('NOK - Пустое поле для ввода имени, NEGATIVE TEST')
+    pass
+
+try:
+    q.negative_test_form(['  ', 'qwe', 'ermqwe', 'Qwertyuiop[1974', 'Qwertyuiop[1974', '+79223332233'])
+    print('OK - Только пробелы в поле для ввода имени, NEGATIVE TEST')
+except:
+    print('NOK - Только пробелы в поле для ввода имени, NEGATIVE TEST')
+    pass
+
+try:
+    q.negative_test_form(['йорптирвнкглРПимхъэжфрыпвагцштиьмофяРЕУфвапясьмдлйь', 'qwe', 'ermqwe', 'Qwertyuiop[1974', 'Qwertyuiop[1974', '+79223332233'])
+    print('OK - 51 символ в поле для ввода имени, NEGATIVE TEST')
+except:
+    print('NOK - 51 символ в поле для ввода имени, NEGATIVE TEST')
+    pass
+
+try:
+    q.negative_test_form(['<script>alert("XSS atack!");</script>', 'qwe', 'ermqwe', 'Qwertyuiop[1974', 'Qwertyuiop[1974', '+79223332233'])
+    print('OK - XSS инекция в поле для ввода имени, NEGATIVE TEST')
+except:
+    print('NOK - XSS инекция в поле для ввода имени, NEGATIVE TEST')
+    pass
+
+try:
+    q.negative_test_form(['105; DROP TABLE Suppliers', 'qwe', 'ermqwe', 'Qwertyuiop[1974', 'Qwertyuiop[1974', '+79223332233'])
+    print('OK - SQL инекция в поле для ввода имени, NEGATIVE TEST')
+except:
+    print('NOK - SQL инекция в поле для ввода имени, NEGATIVE TEST')
+    pass
+
+try:
+    q.negative_test_form(['<style>input[name=csrf_token][value=^a]</style>', 'qwe', 'ermqwe', 'Qwertyuiop[1974', 'Qwertyuiop[1974', '+79223332233'])
+    print('OK - CSS инекция в поле для ввода имени, NEGATIVE TEST')
+except:
+    print('NOK - CSS инекция в поле для ввода имени, NEGATIVE TEST')
+    pass
+
+try:
+    q.negative_test_form(['<body>HTMLtag</body>', 'qwe', 'ermqwe', 'Qwertyuiop[1974', 'Qwertyuiop[1974', '+79223332233'])
+    print('OK - HTML инекция в поле для ввода имени, NEGATIVE TEST')
+except:
+    print('NOK - HTML инекция в поле для ввода имени, NEGATIVE TEST')
+    pass
+# q.positive_test_form(['qwe', 'йцу', 'ermqwe', 'Qwertyuiop[1974', 'Qwertyuiop[1974', '+420773288247'])
 
