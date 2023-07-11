458818ae4a46e50a263d13d7d505740e1e1c8eb2
dener1974 <dener1974@icloud.com>
2023-07-11 19:59:01 +0200

    asddds

--- tests_models.py
+++ tests_models.py
@@ -1,56 +1,78 @@
+import time
+from datetime import datetime
 from pages_models import RegPage
 
 q = RegPage()
 
 # [firstname, lastname, password, password-confirm, phone namber]
+timestamp = datetime.now().timestamp()
+date_time = datetime.fromtimestamp(timestamp)
+str_date_time = str(date_time.strftime("%d-%m-%Y, %H:%M:%S"))
+
+with open('info_log.txt', 'a') as f:
+    f.write(str_date_time + '\n')
 
 try:
-    q.negative_test_form(['', 'qwe', 'ermqwe', 'Qwertyuiop[1974', 'Qwertyuiop[1974', '+79223332233'])
-    print('OK - Пустое поле firstname, NEGATIVE TEST')
+    q.negative_test_form(['asd', 'qwe', 'ermqwe', 'Qwertyuiop[1974', 'Qwertyuiop[1974', '+79223332233'])
+    with open('info_log.txt', 'a') as f:
+        f.write('OK - empty field firstname, NEGATIVE TEST\n')
 except:
-    print('NOK - Пустое поле firstname, NEGATIVE TEST')
+    with open('info_log.txt', 'a') as f:
+        f.write('NOK - empty field firstname, NEGATIVE TEST\n')
     pass
 
 try:
     q.negative_test_form(['  ', 'qwe', 'ermqwe', 'Qwertyuiop[1974', 'Qwertyuiop[1974', '+79223332233'])
-    print('OK - Только пробелы в поле firstname, NEGATIVE TEST')
+    with open('info_log.txt', 'a') as f:
+        f.write('OK - only spaces in field firstname, NEGATIVE TEST\n')
 except:
-    print('NOK - Только пробелы в поле firstname, NEGATIVE TEST')
+    with open('info_log.txt', 'a') as f:
+        f.write('NOK - only spaces in field firstname, NEGATIVE TEST\n')
     pass
 
 try:
     q.negative_test_form(['йорптирвнкглРПимхъэжфрыпвагцштиьмофяРЕУфвапясьмдлйь', 'qwe', 'ermqwe', 'Qwertyuiop[1974', 'Qwertyuiop[1974', '+79223332233'])
-    print('OK - 51 символ в поле firstname, NEGATIVE TEST')
+    with open('info_log.txt', 'a') as f:
+        f.write('OK - 51 simbols in field firstname, NEGATIVE TEST\n')
 except:
-    print('NOK - 51 символ в поле firstname, NEGATIVE TEST')
+    with open('info_log.txt', 'a') as f:
+        f.write('NOK - 51 simbols in field firstname, NEGATIVE TEST\n')
     pass
 
 try:
     q.negative_test_form(['<script>alert("XSS atack!");</script>', 'qwe', 'ermqwe', 'Qwertyuiop[1974', 'Qwertyuiop[1974', '+79223332233'])
-    print('OK - XSS инекция в поле firstname, NEGATIVE TEST')
+    with open('info_log.txt', 'a') as f:
+        f.write('OK - XSS injection in field firstname, NEGATIVE TEST\n')
 except:
-    print('NOK - XSS инекция в поле firstname, NEGATIVE TEST')
+    with open('info_log.txt', 'a') as f:
+        f.write('NOK - XSS injection in field firstname, NEGATIVE TEST\n')
     pass
 
 try:
     q.negative_test_form(['105; DROP TABLE Suppliers', 'qwe', 'ermqwe', 'Qwertyuiop[1974', 'Qwertyuiop[1974', '+79223332233'])
-    print('OK - SQL инекция в поле firstname, NEGATIVE TEST')
+    with open('info_log.txt', 'a') as f:
+        f.write('OK - SQL injection in field firstname, NEGATIVE TEST\n')
 except:
-    print('NOK - SQL инекция в поле firstname, NEGATIVE TEST')
+    with open('info_log.txt', 'a') as f:
+        f.write('NOK - SQL injection in field firstname, NEGATIVE TEST\n')
     pass
 
 try:
     q.negative_test_form(['<style>input[name=csrf_token][value=^a]</style>', 'qwe', 'ermqwe', 'Qwertyuiop[1974', 'Qwertyuiop[1974', '+79223332233'])
-    print('OK - CSS инекция в поле firstname, NEGATIVE TEST')
+    with open('info_log.txt', 'a') as f:
+        f.write('OK - CSS injection in field firstname, NEGATIVE TEST\n')
 except:
-    print('NOK - CSS инекция в поле firstname, NEGATIVE TEST')
+    with open('info_log.txt', 'a') as f:
+        f.write('NOK - CSS injection in field firstname, NEGATIVE TEST\n')
     pass
 
 try:
     q.negative_test_form(['<body>HTMLtag</body>', 'qwe', 'ermqwe', 'Qwertyuiop[1974', 'Qwertyuiop[1974', '+79223332233'])
-    print('OK - HTML инекция в поле firstname, NEGATIVE TEST')
+    with open('info_log.txt', 'a') as f:
+        f.write('OK - HTML injection in field firstname, NEGATIVE TEST\n')
 except:
-    print('NOK - HTML инекция в поле firstname, NEGATIVE test')
+    with open('info_log.txt', 'a') as f:
+        f.write('NOK - HTML injection in field firstname, NEGATIVE TEST\n')
 
 
 # q.positive_test_form(['qwe', 'йцу', 'ermqwe', 'Qwertyuiop[1974', 'Qwertyuiop[1974', '+420773288247'])
