5905c197243a4c505eb154270d6457e64ce5739f
dener1974 <dener1974@icloud.com>
2023-07-26 20:28:34 +0200

    asdsdsda

--- tests_models.py
+++ tests_models.py
@@ -1,15 +1,25 @@
 import time
 from datetime import datetime
 
+list_1 = ['Jan', 'Hus', 'janhus.cz', 'Qwertyuiop[1974', 'Qwertyuiop[1974', '+79223332233']
+list_2 = [['', 'empty field firstname, NEGATIVE TEST'],
+          [' ', 'only spaces in field firstname, NEGATIVE TEST'],
+          ['йорптирвнкглРПимхъэжфрыпвагцштиьмофяРЕУфвапясьмдлйь', '51 simbols in field firstname, NEGATIVE TEST'],
+          ['<script>alert("XSS attack!");</script>', 'XSS injection in field firstname, NEGATIVE TEST'],
+          ['105; DROP TABLE Suppliers', 'SQL injection in field firstname, NEGATIVE TEST'],
+          ['<style>input[name=csrf_token][value=^a]</style>', 'CSS injection in field firstname, NEGATIVE TEST'],
+          ['<body>HTMLtag</body>', 'HTML injection in field firstname, NEGATIVE TEST']]
+
+for element in list_2:
+    list_1[0] = element[0]
+    print(list_1)
+
 def check_list_registration_form(q):
     # [firstname, lastname, password, password-confirm, phone namber]
     timestamp = datetime.now().timestamp()
     date_time = datetime.fromtimestamp(timestamp)
     str_date_time = str(date_time.strftime("%d-%m-%Y, %H:%M:%S"))
-    list_1 = ['sdf', 'qwe', 'ermqwe', 'Qwertyuiop[1974', 'Qwertyuiop[1974', '+79223332233']
-    list_2 = ['', ' ', 'йорптирвнкглРПимхъэжфрыпвагцштиьмофяРЕУфвапясьмдлйь', '<script>alert("XSS atack!");</script>',
-              '105; DROP TABLE Suppliers', '<style>input[name=csrf_token][value=^a]</style>',
-              '<body>HTMLtag</body>']
+
 
 
     with open('info_log.txt', 'a') as f:
@@ -18,6 +28,10 @@ def check_list_registration_form(q):
             f.write('Chrome browser tests' + '\n' + "---------------------------------------------------" + '\n')
         elif 'firefox' in str(q.driver):
             f.write('Firefox browser tests' + '\n' + "---------------------------------------------------" + '\n')
+
+
+
+
 
     try:
         q.negative_test_form(['', 'qwe', 'ermqwe', 'Qwertyuiop[1974', 'Qwertyuiop[1974', '+79223332233'])
@@ -90,3 +104,4 @@ def check_list_registration_form(q):
 
 
 # q.positive_test_form(['qwe', 'йцу', 'ermqwe', 'Qwertyuiop[1974', 'Qwertyuiop[1974', '+420773288247'])
+
